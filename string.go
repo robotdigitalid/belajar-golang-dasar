745ccba6249aa19024dd1974e85efcd9fe81c8b3
Haqi <haqi@buatan.id>
2022-01-03 15:17:12 +0700

    Tipe data string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Haqi")
+	fmt.Println("Ramadhani")
+	fmt.Println("Haqi Ramadhani")
+
+	fmt.Println(len("Haqi"))
+	fmt.Println("Ramadhani"[0])
+	fmt.Println("Haqi Ramadhani"[1])
+}
