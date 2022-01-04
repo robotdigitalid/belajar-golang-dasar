f1c3e82a67df63cb8db91de268ed95f9beda047c
Haqi <haqi@buatan.id>
2022-01-04 10:02:23 +0700

    Operasi perbandingan

---
+++ comparation-operator.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+//goland:noinspection ALL
+func main() {
+	var name1 = "Haqi"
+	var name2 = "Budi"
+
+	var result bool = name1 == name2
+	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+}
