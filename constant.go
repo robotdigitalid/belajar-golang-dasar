ba64f6cac3109e12a43830f88169031c58f98a0c
Haqi <haqi@buatan.id>
2022-01-04 09:25:49 +0700

    Constant

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const value = 1000
+	const (
+		firstName string = "Haqi"
+		lastName         = "Ramadhani"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+}
