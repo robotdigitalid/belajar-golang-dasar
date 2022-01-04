2d532492e0c30cd3109f36f56712c887551f125d
Haqi <haqi@buatan.id>
2022-01-04 10:22:16 +0700

    Tipe data array

--- type-declaration.go
+++ type-declaration.go
@@ -6,7 +6,7 @@ func main() {
 	type NoKTP string
 	type Married bool
 
-	var noKtpHaqi NoKTP = "3328181301990002"
+	var noKtpHaqi NoKTP = "093940283940423"
 	var marriedStatus Married = false
 	fmt.Println(noKtpHaqi)
 	fmt.Println(marriedStatus)
